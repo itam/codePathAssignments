7ba18355032c039d6a9ff8ae34f3f389463129fc
Iria Tam <iria@smugmug.com>
2016-10-14 17:57:24 -0700

    finish pt4 of video tutorial

--- Project1/MovieViewer/MoviesViewController.swift
+++ Project1/MovieViewer/MoviesViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AFNetworking
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
@@ -65,8 +66,15 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         let title = movie?["title"] as? String
         let overview = movie?["overview"] as? String
         
+        let baseUrl = "https://image.tmdb.org/t/p/w500"
+        let posterPath = movie?["poster_path"] as? String
+        
+        let imageUrl = URL(string: baseUrl + posterPath!)
+        
         cell.titleLabel.text = title
         cell.overviewLabel.text = overview
+        
+        cell.posterView.setImageWith(imageUrl!)
 
         return cell
     }
