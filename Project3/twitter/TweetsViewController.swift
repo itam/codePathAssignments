4058a649aa952881fc95c44a59977abc299380cc
Iria Tam <iria@smugmug.com>
2016-10-29 12:24:47 -0700

    tweets view controller skeleton

--- Project3/twitter/TweetsViewController.swift
+++ Project3/twitter/TweetsViewController.swift
@@ -1,19 +1,25 @@
 //
-//  LoginViewController.swift
+//  TweetsViewController.swift
 //  twitter
 //
-//  Created by Iria on 10/27/16.
+//  Created by Iria on 10/29/16.
 //  Copyright © 2016 Iria. All rights reserved.
 //
 
 import UIKit
-import BDBOAuth1Manager
 
-class LoginViewController: UIViewController {
+class TweetsViewController: UIViewController {
+    
+    var tweets: [Tweet]?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        TwitterClient.sharedInstance?.homeTimeline(success: { (tweets: [Tweet]) in
+            self.tweets = tweets
+        }, failure: { (error: Error) in
+            print("error: \(error.localizedDescription)")
+        })
         // Do any additional setup after loading the view.
     }
 
@@ -22,18 +28,6 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func onLoginButton(_ sender: AnyObject) {
-        let client = TwitterClient.sharedInstance
-        
-        client?.login(success: { 
-            print("logged in")
-            
-            self.performSegue(withIdentifier: "loginSegue", sender: nil)
-            
-        }, failure: { (error: Error) in
-            print("error: \(error.localizedDescription)")
-        })
-    }
 
     /*
     // MARK: - Navigation
